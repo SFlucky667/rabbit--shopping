15dd825fcc2adeb60f7ac2bc16731995f773368c
lenovo <sf.lucky667@gmail.com>
2025-10-15 17:51:58 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/main.css'
+
+//测试接口函数
+import {getCategory} from '@/api/testAPI'
+getCategory().then(res=>{
+    console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
