b2adb076fcaca978f78f2364e3e1b48d8d2f0f45
lenovo <sf.lucky667@gmail.com>
2025-10-17 13:58:09 +0800

    'banner轮播图实现'

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElCarousel: typeof import('element-plus/es')['ElCarousel']
+    ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
