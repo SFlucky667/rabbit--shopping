b2f44744c4b8c0987a5c922931cb09a10f0cea0a
lenovo <sf.lucky667@gmail.com>
2025-10-17 15:24:58 +0800

    新鲜好物和人气推荐实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,7 +5,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeCategory from './components/HomeCategory.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeNew from './components/HomeNew.vue'
-import HomePanal from './components/HomePanal.vue'
 import homeProduct from './components/HomeProduct.vue'
 </script>
 
@@ -18,10 +17,10 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeHot />
   <homeProduct />
   <!-- 测试面板组件 -->
-   <HomePanal title="新鲜好物" subTitle="新鲜出炉的商品">
+   <!-- <HomePanal title="新鲜好物" subTitle="新鲜出炉的商品">
      <div>我是新鲜好物的插槽内容</div>
 </HomePanal>
 <HomePanal title="人气推荐" subTitle="人气商品">
     <div>我是人气推荐的插槽内容</div>
-  </HomePanal>
+  </HomePanal> -->
 </template>
