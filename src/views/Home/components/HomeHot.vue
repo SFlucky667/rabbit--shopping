51bd6fe80d3e276059fd41a7fb2ce146091fec55
lenovo <sf.lucky667@gmail.com>
2025-10-20 11:21:35 +0800

    home-图片懒加载指令优化

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -3,6 +3,7 @@
 import { getHotAPI } from "@/api/home";
 import { ref } from "vue";
 import HomePanal from './HomePanal.vue';
+// 定义懒加载插件
 
 const hotList = ref([]);
 const getHotList = async () => {
@@ -18,7 +19,7 @@ getHotList();
       <ul class="goods-list">
         <li v-for="item in hotList" :key="item.id">
           <RouterLink to="/">
-            <img :src="item.picture" alt="" />
+            <img v-img-lazy="item.picture"  alt="" />
             <p class="name">{{ item.title }}</p>
             <p class="desc">{{ item.alt }}</p>
           </RouterLink>
