1dfd454510793352d4d7fb4998537ca52ea240db
lenovo <sf.lucky667@gmail.com>
2025-10-21 17:01:58 +0800

    一级分类-轮播图功能实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,9 +2,10 @@
 <!-- eslint-disable vue/block-lang -->
 <script setup>
   import { getCategoryAPI } from '@/api/category'
+import { getBannerAPI } from '@/api/home'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
-  //获取数据
+//获取数据
   const categoryData=ref({})
   //如何在组件内部获取路由参数等
   const route=useRoute()
@@ -13,6 +14,19 @@ import { useRoute } from 'vue-router'
     categoryData.value=res.result
   }
   onMounted(()=>getCategory())
+  //获取banner
+  const bannerList=ref([])
+
+const getBanner=async()=>{
+      const res=await getBannerAPI({
+        distributionSite:'2'
+      })
+      console.log(res)
+      bannerList.value=res.result
+}
+onMounted(()=>{
+  getBanner()
+})
 </script>
 
 <template>
@@ -25,6 +39,14 @@ import { useRoute } from 'vue-router'
           <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
     </div>
   </div>
 </template>
@@ -107,5 +129,15 @@ import { useRoute } from 'vue-router'
   .bread-container {
     padding: 25px 0;
   }
+  .home-banner {
+  width: 1240px;
+  height: 500px;
+margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 }
 </style>
--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -14,6 +14,8 @@ const getBanner=async()=>{
 onMounted(()=>{
   getBanner()
 })
+
+
 </script>
 
 
