dd1ad02cd768f60586dde095f3314e21625b3d64
lenovo <sf.lucky667@gmail.com>
2025-10-21 18:24:16 +0800

    一级分类-激活状态控制和分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -5,6 +5,7 @@
 import { getBannerAPI } from '@/api/home'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/Goodsltem.vue'
 //获取数据
   const categoryData=ref({})
   //如何在组件内部获取路由参数等
@@ -47,6 +48,26 @@ onMounted(()=>{
       </el-carousel-item>
     </el-carousel>
   </div>
+  <div class="sub-list">
+  <h3>全部分类</h3>
+  <ul>
+    <li v-for="i in categoryData.children" :key="i.id">
+      <RouterLink to="/">
+        <img :src="i.picture" />
+        <p>{{ i.name }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
+<div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+  <div class="head">
+    <h3>- {{ item.name }}-</h3>
+  </div>
+  <div class="body">
+    <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+  </div>
+</div>
+
     </div>
   </div>
 </template>
