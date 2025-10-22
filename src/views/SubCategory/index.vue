ec8f4bade1297e8e729c25fce43c1437da247e1b
lenovo <sf.lucky667@gmail.com>
2025-10-22 19:22:56 +0800

    二级分类-添加筛选参数实现筛选功能

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -28,6 +28,14 @@ onMounted(()=>getCategoryData())
   goodList.value=res.result.items
  }
  onMounted(()=>getGoodList())
+
+ //tab切换回调
+ const tabChange=()=>{
+  console.log('tab切换了',reqData.value.sortField)
+  reqData.value.page=1
+  getGoodList()
+ }
+  
 </script>
 
 <template>
@@ -41,7 +49,7 @@ onMounted(()=>getCategoryData())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortFild" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
