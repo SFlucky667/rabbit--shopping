6c132d3452a7de2a989378abc65bd8d1375eb7f2
lenovo <sf.lucky667@gmail.com>
2025-10-23 13:30:24 +0800

    详情页-整体认识和路由配置

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -19,7 +19,7 @@ onMounted(()=>{
   <HomePanal title="新鲜好物" subTitle="新鲜出炉的商品">
    <ul class="goods-list">
     <li v-for="item in newList" :key="item.id">
-      <RouterLink to="/">
+      <RouterLink :to="`/detail/${item.id}`">
         <img :src="item.picture" alt="" />
         <p class="name">{{ item.name }}</p>
         <p class="price">&yen;{{ item.price }}</p>
