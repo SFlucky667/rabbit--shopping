dd04897359059d2f2d41ea584790431dbc768638
lenovo <sf.lucky667@gmail.com>
2025-10-29 11:30:11 +0800

    购物车-列表购物车基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from '@/views/Login/index.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和component对应关系的位置
@@ -30,6 +31,14 @@ const router = createRouter({
          {
           path:'detail/:id',
           component:Detail
+        },
+         {
+          path:'detail/:id',
+          component:Detail
+        },
+         {
+          path:'/cartlist',
+          component:CartList
         }
       ]
     },
